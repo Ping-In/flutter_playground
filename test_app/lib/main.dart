85053ad819257f65a2dbec11b840f9e396f725cc
Krommenhoek Niels, SUB-SDC-NL-MBM-BNC <Niels.Krommenhoek1@swisscom.com>
2024-01-17 22:27:15 +0100

    Tried more widgets - logger dependecy added

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,13 +1,22 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  List names = ['Niels', 'Sharon', 'Mike', 'Conner'];
+
+  var logger = Logger();
+
+  void userTapped() {
+    logger.i("User tapped");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +26,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text("Ping-In"),
-          backgroundColor: Color.fromARGB(255, 62, 181, 54),
+          backgroundColor: Color.fromARGB(255, 6, 255, 14),
           centerTitle: true,
           leading: IconButton(
             icon: const Icon(Icons.menu),
@@ -27,27 +36,123 @@ class MyApp extends StatelessWidget {
             IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
           ],
         ),
-        body: Center(
+        body: GestureDetector(
+          onTap: () {
+            userTapped();
+          },
           child: Container(
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 62, 181, 54),
-                  borderRadius: BorderRadius.circular(20)),
-              padding: EdgeInsets.all(50),
-              child: Icon(
-                Icons.map_outlined,
-                size: 50,
-              )
-              // child: Text(
-              //   "Ping-In",
-              //   style: TextStyle(
-              //       color: Colors.white,
-              //       fontSize: 28,
-              //       fontWeight: FontWeight.bold),
-              // ),
-              ),
+            height: 200,
+            width: 200,
+            color: Colors.green,
+            child: Center(child: Text("Tap me!")),
+          ),
         ),
+
+        // body: Stack(
+        //   alignment: Alignment.center,
+        //   children: [
+        //     // Big Box
+        //     Container(
+        //       height: 300,
+        //       width: 300,
+        //       color: Colors.green,
+        //     ),
+        //     Container(
+        //       height: 200,
+        //       width: 200,
+        //       color: Colors.green[400],
+        //     ),
+        //     Container(
+        //       height: 100,
+        //       width: 100,
+        //       color: Colors.green[200],
+        //     )
+        //   ],
+        // )
+
+        // body: GridView.builder(
+        //     itemCount: 64,
+        //     gridDelegate:
+        //         SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+        //     itemBuilder: (context, index) => Container(
+        //           color: Colors.green,
+        //           margin: EdgeInsets.all(2),
+        //         )),
+
+        // body: ListView.builder(
+        //   itemCount: names.length,
+        //   itemBuilder: (context, index) => ListTile(title: Text(names[index])),
+        // ),
+
+        // body: ListView(
+        //   children: [
+        //     // 1st Box
+        //     Container(
+        //       height: 350,
+        //       color: Colors.green,
+        //     ),
+
+        //     // 2st Box
+        //     Container(
+        //       height: 350,
+        //       color: Colors.green[400],
+        //     ),
+
+        //     // 3rd Box
+        //     Container(
+        //       height: 350,
+        //       color: Colors.green[200],
+        //     ),
+        //   ],
+        // )
+
+        // body: Column(
+        //   children: [
+        //     // 1st Box
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.green,
+        //       ),
+        //     ),
+
+        //     // 2st Box
+        //     Expanded(
+        //       flex: 2,
+        //       child: Container(
+        //         color: Colors.green[400],
+        //       ),
+        //     ),
+
+        //     // 3rd Box
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.green[200],
+        //       ),
+        //     )
+        //   ],
+        // )
+
+        // body: Center(
+        //   child: Container(
+        //       height: 150,
+        //       width: 150,
+        //       decoration: BoxDecoration(
+        //           color: Color.fromARGB(255, 62, 181, 54),
+        //           borderRadius: BorderRadius.circular(20)),
+        //       padding: EdgeInsets.all(50),
+        //       child: Icon(
+        //         Icons.map_outlined,
+        //         size: 50,
+        //       )
+        //       // child: Text(
+        //       //   "Ping-In",
+        //       //   style: TextStyle(
+        //       //       color: Colors.white,
+        //       //       fontSize: 28,
+        //       //       fontWeight: FontWeight.bold),
+        //       // ),
+        //       ),
+        // ),
       ),
     );
   }
