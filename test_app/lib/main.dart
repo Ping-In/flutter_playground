82efd3f158f9afe993e33011a00d9d6d71979ab5
Krommenhoek Niels, SUB-SDC-NL-MBM-BNC <Niels.Krommenhoek1@swisscom.com>
2024-01-19 17:00:43 +0100

    Started login screen

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,38 +1,20 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable
-
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
-import 'package:test_app/pages/first_page.dart';
-import 'package:test_app/pages/home_page.dart';
-import 'package:test_app/pages/second_page.dart';
-import 'package:test_app/pages/settings_page.dart';
+import 'package:test_app/pages/login_page.dart';
+import 'package:test_app/themes/light_mode.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  List names = ['Niels', 'Sharon', 'Mike', 'Conner'];
-
-  var logger = Logger();
-
-  void userTapped() {
-    logger.i("User tapped");
-  }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-      routes: {
-        '/firstPage': (context) => FirstPage(),
-        '/secondPage': (context) => SecondPage(),
-        '/homePage': (context) => HomePage(),
-        '/settingsPage': (context) => SettingsPage(),
-      },
+      home: LoginPage(),
+      theme: lightMode,
     );
   }
 }
---
+++ test_app/lib/pages/login_page.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:test_app/components/text_field.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset("images/pingin.png", fit: BoxFit.cover),
+            const SizedBox(height: 50),
+            Text(
+              "Welcome to Ping-In!",
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 50),
+            MyTextField(
+              hintText: "Email",
+              obscureText: true,
+            ),
+            const SizedBox(height: 25),
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
